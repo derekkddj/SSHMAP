0378c8a964227a3c7b828f716e6093ffc6865fd9
dhd0004 <derekkddj@gmail.com>
2025-05-02 00:27:58 +0200

    tests and docker compose

---
+++ tests/conftest.py
@@ -0,0 +1,8 @@
+import pytest
+from modules.credential_store import CredentialStore
+
+@pytest.fixture
+def dummy_store():
+    store = CredentialStore()
+    store.store("127.0.0.1", 22, "testuser", "testpass", "password")
+    return store
