4557abde7a10b6ae931f2aff9545fe9fa934ee45
dhd0004 <derekkddj@gmail.com>
2025-05-04 12:47:58 +0200

    create default config file if it does not exists

--- modules/config.py
+++ modules/config.py
@@ -3,8 +3,24 @@ from .paths import SSHMAP_PATH
 
 class Config:
     def __init__(self, path=f"{SSHMAP_PATH}/config.yml"):
-        with open(path, "r") as f:
-            self._config = yaml.safe_load(f)
+        # If file does not exist, create it with default values
+        try:
+            with open(path, "r") as f:
+                self._config = yaml.safe_load(f)
+        except FileNotFoundError:
+            # Create a default config file if it doesn't exist and save it
+            self._config = {
+                "neo4j_uri": "bolt://localhost:7687",
+                "neo4j_user": "neo4j",
+                "neo4j_password": "neo4j",
+                "max_mask": 24,
+                "ssh_ports": [22,2222,2223],
+                "max_depth": 3,
+                "scan_timeout": 5,
+                "brute_new_credentials": False
+            }
+            with open(path, "w") as f:
+                yaml.dump(self._config, f)
 
     def __getitem__(self, key):
         return self._config[key]
