0feb42e2bca8dd5af24fa9512a44e044bc91e8d5
derekkddj <derekkddj@gmail.com>
2025-05-06 15:27:38 +0200

    exception correction

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ from modules.credential_store import CredentialStore
 
 
 @pytest.fixture
+@pytest.mark.asyncio
 def dummy_store():
     store = CredentialStore()
     store.store("127.0.0.1", 22, "testuser", "testpass", "password")
